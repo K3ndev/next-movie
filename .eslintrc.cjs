10dbebb57d41aaf54257bef244cc2a92276f042b
k3n <racelisjk@gmail.com>
2023-03-08 04:29:55 +0800

    seting up lint

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+// before
+// {
+//   "extends": "next/core-web-vitals"
+// }
+
+module.exports = {
+  parserOptions: {
+    project: ['./tsconfig.json'],
+  },
+  extends: [
+    'next/core-web-vitals',
+    'airbnb-base',
+    'airbnb-typescript',
+    'plugin:prettier/recommended',
+  ],
+};
