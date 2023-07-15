f2c7cceea5d24bbb1fc72b5320d8b5b809f9c162
K3ndev <racelisjk@gmail.com>
2023-07-16 03:41:36 +0800

    feat: add searchParam state and handleChnage fn

--- src/components/custom/SearchMenu/index.tsx
+++ src/components/custom/SearchMenu/index.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { ChevronDown } from "lucide-react";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Button } from "@/components/ui/button";
@@ -31,7 +32,72 @@ const HealthList = [
 ];
 const MealTypeList = ["Breakfast", "Dinner", "Lunch", "Snack", "Teatime"];
 
+type SearchParamState = {
+  DietList: { [key: string]: boolean };
+  HealthList: { [key: string]: boolean };
+  MealTypeList: { [key: string]: boolean };
+};
+
 export function SearchMenu() {
+  const [searchParam, setSearchParam] = useState<SearchParamState>({
+    DietList: {},
+    HealthList: {},
+    MealTypeList: {},
+  });
+
+  // setup the searchParam
+  useEffect(() => {
+    const initialDietList: SearchParamState["DietList"] = {};
+    const initialHealthList: SearchParamState["HealthList"] = {};
+    const initialMealTypeList: SearchParamState["MealTypeList"] = {};
+
+    DietList.forEach((diet) => {
+      initialDietList[diet] = false;
+    });
+
+    HealthList.forEach((health) => {
+      initialHealthList[health] = false;
+    });
+
+    MealTypeList.forEach((mealType) => {
+      initialMealTypeList[mealType] = false;
+    });
+
+    setSearchParam({
+      DietList: initialDietList,
+      HealthList: initialHealthList,
+      MealTypeList: initialMealTypeList,
+    });
+  }, []);
+
+  const handleDietChange = (diet: string) => {
+    setSearchParam((prevState) => ({
+      ...prevState,
+      DietList: {
+        ...prevState.DietList,
+        [diet]: !prevState.DietList[diet],
+      },
+    }));
+  };
+  const handleHealthChange = (diet: string) => {
+    setSearchParam((prevState) => ({
+      ...prevState,
+      HealthList: {
+        ...prevState.HealthList,
+        [diet]: !prevState.HealthList[diet],
+      },
+    }));
+  };
+  const handleMealTypeChange = (diet: string) => {
+    setSearchParam((prevState) => ({
+      ...prevState,
+      MealTypeList: {
+        ...prevState.MealTypeList,
+        [diet]: !prevState.MealTypeList[diet],
+      },
+    }));
+  };
+
   return (
     <HoverCard openDelay={3}>
       <HoverCardTrigger asChild>
@@ -49,7 +115,11 @@ export function SearchMenu() {
                 {DietList.map((item, index) => {
                   return (
                     <div className="flex items-center space-x-1" key={index}>
-                      <Checkbox id={item} />
+                      <Checkbox
+                        id={item}
+                        checked={searchParam.DietList[`${item}`]}
+                        onClick={() => handleDietChange(item)}
+                      />
                       <label
                         htmlFor={item}
                         className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer select-none"
@@ -67,7 +137,11 @@ export function SearchMenu() {
                 {HealthList.map((item, index) => {
                   return (
                     <div className="flex items-center space-x-1" key={index}>
-                      <Checkbox id={item} />
+                      <Checkbox
+                        id={item}
+                        checked={searchParam.HealthList[`${item}`]}
+                        onClick={() => handleHealthChange(item)}
+                      />
                       <label
                         htmlFor={item}
                         className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 select-none cursor-pointer"
@@ -85,7 +159,11 @@ export function SearchMenu() {
                 {MealTypeList.map((item, index) => {
                   return (
                     <div className="flex items-center space-x-1" key={index}>
-                      <Checkbox id={item} />
+                      <Checkbox
+                        id={item}
+                        checked={searchParam.MealTypeList[`${item}`]}
+                        onClick={() => handleMealTypeChange(item)}
+                      />
                       <label
                         htmlFor={item}
                         className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 select-none cursor-pointer"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,24 @@ export default function Home() {
   // const { DATA } = useInfiniteFetch({ entry, url });
 
   // console.log(DATA);
+  // https://api.edamam.com/api/recipes/v2?field=label&field=images&field=source&field=dietLabels&field=url&field=healthLabels&field=ingredientLines&field=ingredients&field=calories&field=totalWeight&field=totalTime&field=cuisineType&field=mealType&field=dishType&field=cautions&field=uri
+  // &eq=bef&type=public&app_id=${APPLICATIONID}&app_key=${APPLICATIONKEYS}
 
+  // https://api.edamam.com/api/recipes/v2?field=label&field=images&field=source&field=dietLabels&field=url&field=healthLabels&field=ingredientLines&field=ingredients&field=calories&field=totalWeight&field=totalTime&field=cuisineType&field=mealType&field=dishType&field=cautions&field=uri
+  // &diet=balanced
+  // &q=beef&type=public&app_id=338fbe36&app_key=71b0be31e06ed9b486f4758f0a72949e
+
+  // https://api.edamam.com/api/recipes/v2?field=label&field=images&field=source&field=dietLabels&field=url&field=healthLabels&field=ingredientLines&field=ingredients&field=calories&field=totalWeight&field=totalTime&field=cuisineType&field=mealType&field=dishType&field=cautions&field=uri
+  // &diet=balanced&health=alcohol-free&q=beef
+  // &type=public&app_id=338fbe36&app_key=71b0be31e06ed9b486f4758f0a72949e
+
+  // https://api.edamam.com/api/recipes/v2?field=label&field=images&field=source&field=dietLabels&field=url&field=healthLabels&field=ingredientLines&field=ingredients&field=calories&field=totalWeight&field=totalTime&field=cuisineType&field=mealType&field=dishType&field=cautions&field=uri
+  // &diet=balanced
+  // &diet=high-fiber
+  // &mealType=Breakfast
+  // &health=alcohol-free
+  // &q=beef
+  // &type=public&app_id=338fbe36&app_key=71b0be31e06ed9b486f4758f0a72949e
   return (
     <Layout>
       <GenericPanel>
