506729ff6d8e37564a5377067775113b67ebb207
K3ndev <racelisjk@gmail.com>
2023-09-13 12:43:25 +0800

    remove: unused import

--- src/modules/home/components/pokemonList/Pokemons.tsx
+++ src/modules/home/components/pokemonList/Pokemons.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useFetchPokemon } from '../../hooks/index';
-import ImageLoader from "./loader"
 
 type PokemonType = {
   pokemonURL: string;
